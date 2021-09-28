e74de49d90c35d0f28fad9bb0b83e0d9448cea31
iantendu <iantendu@gmail.com>
2021-09-28 12:49:39 +0300

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+
+        const val API_KEY="474305572be0407588de96d799289ce0"
+        const val BASE_URL="https://newsapi.org/"
+        const val SEARCH_NEWS_DELAY_TIME=500L
+        const val QUERY_PAGE_SIZE=20
+
+    }
+}
